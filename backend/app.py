53c9edcb467067477b99b7aa5ad2ad8bb7bfb088
MukuFlash03 <mukulm2010@gmail.com>
2023-11-23 20:31:10 -0700

    Changing json data returned for POST request.

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, flash, request, redirect, url_for, render_template
+from flask import Flask, flash, request, jsonify, redirect, url_for, render_template
 import pandas as pd
 
 app = Flask(__name__)
@@ -8,31 +8,41 @@ matric_factorization_df = pd.read_csv('./file2.csv')
 knn_df = pd.read_csv('./file1.csv')
 dff = pd.read_csv("./images_url.csv", encoding = "ISO-8859-1")
 
-@app.route('/homepage', methods=['GET'])
-def homepage():
-    return render_template('index.html')
+# @app.route('/homepage', methods=['GET'])
+# def homepage():
+#     return render_template('index.html')
 
-@app.route('/show_recommendation', methods=['POST'])
-def show_recommendation():
+@app.route('/fetch_recommend', methods=['POST'])
+def fetch_recommendation():
     try:
         user_id = int(request.form['user_id'])
-        if 0<user_id<611:
+
+        if 0 <user_id <611:
             matrix_data = list(matric_factorization_df.iloc[user_id-1])
             knn_data = list(knn_df.iloc[user_id-1])
             data2 = dff.values.tolist()
+
             matrix_send_data = []
             knn_send_data = []
+
             for x in matrix_data:
                 for y in data2:
                     if str(x) in y:
-                        print(y[1])
-                        matrix_send_data.append(y[1])
+                        print([y[0], y[1]])
+                        matrix_send_data.append([y[0], y[1]])
             for x in knn_data:
                 for y in data2:
                     if str(x) in y:
-                        print(y[1])
-                        knn_send_data.append(y[1])
-            return render_template('portfolio-details.html', matrix_data=matrix_send_data, knn_data =knn_send_data )
+                        print([y[0], y[1]])
+                        knn_send_data.append([y[0], y[1]])
+            
+            user_movies = {
+                "matrix_fact": matrix_send_data,
+                "knn": knn_send_data
+            }
+
+            return jsonify(user_movies)
+            # return render_template('portfolio-details.html', matrix_data=matrix_send_data, knn_data =knn_send_data )
         else:
             raise UserIDException(user_id)
     except UserIDException as e:
