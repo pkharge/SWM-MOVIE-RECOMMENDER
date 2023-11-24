b22cd8b9ab7f645e53e086859ddf36d64cac64de
Bhaskar Kumar <bhaskarkr0811@gmail.com>
2023-11-23 19:48:11 -0700

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,156 @@
-import logo from './logo.svg';
 import './App.css';
+import backgroundImage from './images/bg1.jpeg'
+import {TextField, Card, Button, Box, Typography, Modal, Paper} from "@mui/material";
+import Grid2 from "@mui/material/Unstable_Grid2";
+import {red500, redA700} from "mui/source/styles/colors";
+import {useState} from "react";
 
 function App() {
+
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => {
+        console.log("asdfasdf");
+        setOpen(true);
+    }
+    const handleClose = () => setOpen(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div style = {{
+      backgroundImage: `url(${backgroundImage})`,
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      width: '100vw',
+      height: '100vh',
+        display: 'flex',
+        justifyContent:'center',
+        alignItems:'center',
+        verticalAlign:"middle"
+    }}>
+
+        <Grid2
+            container
+            direction="column"
+            spacing={3}
+            alignItems="center"
+
         >
-          Learn React
-        </a>
-      </header>
+            <Grid2 item xs>
+                <Card
+                    sx={{
+                        backgroundColor: 'white',
+                        width: '25vw',
+                        height: 40,
+                        padding: '5px'
+                    }} variant = "outlined" >
+                    <TextField
+                        style = {{backgroundColor: 'white', width: '100%', alignContent: 'center'}}
+                        id="outlined-size-small"
+                        label="Please enter user id"
+                        size="small"
+                        type={'text'}
+                    />
+                </Card>
+            </Grid2>
+        <Grid2 item s>
+            <Button
+                style={{
+                width: '200px',
+                backgroundColor: red500,
+                '&:hover': {
+                    backgroundColor: redA700,
+                }}}
+                variant="contained"
+                onClick={handleOpen}
+            >
+                Recommend Movie
+            </Button>
+        </Grid2>
+        </Grid2>
+        <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+        >
+            <Box sx={{
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)',
+                width: '80vw',
+                height: '80vh',
+                bgcolor: 'background.paper',
+                border: '9px double #FF0000',
+                boxShadow: 24,
+                p: 5,
+
+            }} >
+                <div className="modal-content" >
+                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                        <h1>Matrix Factorization</h1>
+                    </Typography>
+                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                        <Grid2 item xs={12}>
+                            <Grid2 container justifyContent="center" spacing={3}>
+                                {[0, 1, 2,3,4,5,6,7,8,9].map((value) => (
+                                    <Grid2 key={value} item>
+                                        <Paper
+                                            sx={{
+                                                height: 140,
+                                                width: '10vw',
+                                                backgroundColor: (theme) =>
+                                                    theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+                                            }}
+                                        />
+                                    </Grid2>
+                                ))}
+                            </Grid2>
+                        </Grid2>
+                    </Typography>
+                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                        <h1>KNN</h1>
+                    </Typography>
+                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                        <Grid2 item xs={12}>
+                            <Grid2 container justifyContent="stretch" spacing={3}>
+                                {[0, 1, 2,3,4,5,6,7,8,9].map((value) => (
+                                    <Grid2 key={value} item>
+                                        <Paper
+                                            sx={{
+                                                height: 160,
+                                                width: '10vw',
+                                                backgroundColor: (theme) =>
+                                                    theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+                                            }}
+                                        />
+                                    </Grid2>
+                                ))}
+                            </Grid2>
+                        </Grid2>
+                    </Typography>
+                    <Button
+                        style={{
+                            position: 'absolute',
+                            bottom: '5%',
+                            left: '42%',
+                            width: '200px',
+                            backgroundColor: red500,
+                            '&:hover': {
+                                backgroundColor: redA700,
+                            }}}
+                        variant="contained"
+                        onClick = {handleClose}
+                    >
+                        Close
+                    </Button>
+                </div>
+
+
+            </Box>
+
+        </Modal>
+
     </div>
   );
 }
